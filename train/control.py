036456f768d09df25e9b2a8c4da74365d3425ad9
zhaochen20 <zhaochenyang20@gmail.com>
2023-08-04 16:27:09 -0400

    add bart

--- train/control.py
+++ train/control.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 logging_root = Path("logs")
 logging_root.mkdir(parents=True, exist_ok=True)
-model_name_list = ["google/flan-t5-base", "google/mt5-base"]
+model_name_list = ["google/flan-t5-base", "google/mt5-base", "facebook/bart-large-cnn"]
 task_name_list = ["normalization", "NQ", "Chinese2SQL", "jp2python"]
 
 # domenicrosati/QA2D-t5-base, lmqg/flan-t5-base-squad-qag for NQ
 
-for model_name, task_name in product(model_name_list, task_name_list):
+for model_name, task_name in product(["facebook/bart-large-cnn"], ["normalization"]):
     model_store_name = model_name.split("/")[-1]
     logging_path = logging_root / f"{model_store_name}_{task_name}_real_test.log"
     command = f"sbatch --output={str(logging_path)} --gres=gpu:A6000:1 --mem=32G --time 20:00:00 main.sh {model_name} {task_name}"
